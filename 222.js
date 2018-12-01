ae2290ee00fc5238f9d70c704f02c544c48506b6
AdorableLi <1334349017@qq.com>
2018-12-01 21:00:38 +0800

    commit use vscode

--- 222.js
+++ 222.js
@@ -12,6 +12,36 @@ for (var i = 0; i < 10; ++i) {
 // 	alert(2/0);
 // }
 
+function qqq(){
+	const ddd = 2;
+	const fff = document.getElementById("constdiv");
+	fff.innerHTML=ddd;
+
+	// const定义的对象不可以改变值，以下写法会报错
+	// const fff = document.getElementById("constdiv").innerHTML="222";
+	// ddd = fff;
+};
+
+let x = 0;
+function test(){
+	x++;
+	if(x>10){
+		return
+	};
+	console.log("递归：" + x);
+	test();
+}
+
+function byE40() {
+    //nish 在这里是有效的
+console.log("before"+nish);
+  for( var nish = 0; nish < 5; nish++ ) {
+        //nish在这里是有效的
+    };
+    //nish在这里是有效的
+console.log("after"+nish);
+};
+
 
 // JSON数组
 // {
@@ -30,3 +60,31 @@ for (var i = 0; i < 10; ++i) {
 // 		}
 // 	]
 // }
+
+
+
+ res.data.data.tech.map((value, index) => {
+          console.log(value.category)
+          let optionname = value.category;
+          newvoteoption.push(optionname);
+        })
+        that.setData({
+          voteoption: newvoteoption,
+        })
+        console.log(that.data.voteoption);
+optionname => "科技"
+       newvoteoption =>  []
+
+        newvoteoption =>  ["科技"]
+
+        newvoteoption =>  ["科技","科技"]  数组
+        item =》 newvoteoption【0】  =>  "科技"
+
+        newvoteoption => [
+        	{
+        		name:"科技"
+        	}
+        ]   json数组
+        item =》 newvoteoption【0】  =>  {name:"科技"}
+        item.name =》 newvoteoption【0】.name  =>  "科技"
+
