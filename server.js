c8b3bc023d191fb5d42a5bdf7b7dd3ef0407b35c
lishuyu <1684856556@qq.com>
2019-01-16 18:03:14 +0800

    添加数个demo页面，包括分页、复选框、nodejs简易程序

---
+++ server.js
@@ -0,0 +1,20 @@
+//	简单的node.js程序
+
+var http = require('http');
+
+http.createServer(function (request, response) {
+
+    // 发送 HTTP 头部 
+    // HTTP 状态值: 200 : OK
+    // 内容类型: text/plain
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+
+    // 发送响应数据 "Hello World"
+    response.end('Hello World\n');
+}).listen(8888);
+
+// 终端打印如下信息
+console.log('Server running at http://127.0.0.1:8888/');
+
+
+//	terminal中输入node server.js，浏览器中输入127.0.0.1:8888即可运行页面
